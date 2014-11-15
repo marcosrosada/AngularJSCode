3ecdb8ba98e724cfdd2dc81840b29b5c7f5e37a4
Marcos Rosada <ochld@gmail.com>
2014-11-15 19:08:55 +0000

    New features

---
+++ module/second/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html ng-app="store">
+    <head>
+        <link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css" />
+    </head>
+    
+    <body ng-controller="StoreController as store">
+        
+        <div class="list-group" ng-repeat="item in store.produto">
+            
+            <div class="list-group-item" ng-hide="item.vendido">
+                <h1>{{item.name}}</h1>
+                <em class="pull-right" >{{item.preco | currency }}</em>
+                <p>{{item.descricao}}</p>
+                <button ng-show="item.btnEnabled"> Add no carrinho +</button>
+            </div>
+            
+        </div>
+        <script type="text/javascript" src="./js/angular.min.js"></script>
+        <script type="text/javascript" src="./js/app.js"></script>
+    </body>
+</html>
